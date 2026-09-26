fe2297ad6adf76656887cc1bc46dd6322f0631ca
Aary Potdar <aarypotdar01@gmail.com>
2026-09-26 19:35:29 +0530

    Add palindrome number checker

---
+++ 02-conditionals-and-loops/18-palindrome-number.py
@@ -0,0 +1,18 @@
+num = int(input("Enter a number: "))
+
+original = num
+num = abs(num)
+
+reverse = 0
+
+while num > 0:
+    digit = num % 10
+    reverse = reverse * 10 + digit
+    num = num // 10
+
+if original >= 0 and original == reverse:
+    print("Palindrome")
+else:
+    print("Not a Palindrome")
+
+
